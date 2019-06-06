10bedb31cce9194c9c51902b609ac278f59f2931
caiyou <rodickcai@gmail.com>
2019-06-06 15:40:26 +0800

    UPD

---
+++ parseXlsx.js
@@ -0,0 +1,27 @@
+/*
+    将Excel简化成json写入文件
+*/
+
+const XLSX = require('xlsx')
+const fs = require('fs')
+const workbook = XLSX.readFile('./taihe.xlsx')
+const SheetNamesArr = workbook.SheetNames
+
+for (let j = 0; j < SheetNamesArr.length; j++) {
+    let sheet = workbook.Sheets[SheetNamesArr[j]]
+    let sheetMap = []
+
+    let i = 3
+    while (sheet[`F${i}`]) {
+        sheetMap.push({
+            artist: sheet[`E${i}`]['v'],
+            song: sheet[`F${i}`]['v']
+        })
+        i++
+    }
+    console.log(sheetMap)
+    fs.writeFile(`${SheetNamesArr[j]}.json`, JSON.stringify(sheetMap), 'utf8', (err) => {
+        if (err) throw err
+        console.log('done')
+    })
+}
--- xiaoniangao.js
+++ xiaoniangao.js
@@ -1,15 +1,37 @@
 const axios = require('axios')
+const fs = require('fs')
+let count = 15
 
-axios.post('https://api.xiaoniangao.cn/plp/searchall', {
-    "txt": "林俊杰",
-    "offset": 0,
-    "limit": 50,
-    "token": "551a07468884ee39a00f87df2d7a507c",
-    "uid": "832daf83-1c2d-4966-a325-78b06b397069",
-    "proj": "ma",
-    "wx_ver": "7.0.4",
-    "code_ver": "1.13.0"
-}).then(res => {
-    const list = res.data.data.list
-    console.log(list)
+fs.readFile('./taihe.json', 'utf8', function (err, data) {
+    if (err) throw err
+    const infoArr = JSON.parse(data)
+    infoArr.forEach((el, index) => {
+        if (index >= 1000 && index < 2000) {
+            detect(el['artist'], el['song'])
+        }
+    })
 })
+
+async function detect(artist, song) { 
+    let res = await axios.post('https://api.xiaoniangao.cn/plp/searchall', {
+        "txt": song,
+        "offset": 0,
+        "limit": 50,
+        "token": "551a07468884ee39a00f87df2d7a507c",
+        "uid": "832daf83-1c2d-4966-a325-78b06b397069",
+        "proj": "ma",
+        "wx_ver": "7.0.4",
+        "code_ver": "1.13.0"
+    })
+    if (res.data.data && res.data.data.list.length > 0) {
+        const list = res.data.data.list
+        if (list[0]['music']['name'].indexOf(song) !== -1 && list[0]['music']['singer'].indexOf(artist) !== -1) {
+            console.log(`【${++count}】${artist} - ${song}`)
+            // hitHaidie.push({
+            //     artist,song
+            // })
+        }
+    }
+}
+
+
