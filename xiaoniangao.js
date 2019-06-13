b6849331c84c4a84440bf0a66981ee386446cebb
caiyou <rodickcai@gmail.com>
2019-06-13 16:18:59 +0800

    UPDATE 错误捕获

--- xiaoniangao.js
+++ xiaoniangao.js
@@ -2,34 +2,42 @@ const axios = require('axios')
 const fs = require('fs')
 let count = 0
 
-fs.readFile('./parse/合众.json', 'utf8', function (err, data) {
+fs.readFile('./parse/天狼.json', 'utf8', function (err, data) {
     if (err) throw err
     const infoArr = JSON.parse(data)
+    // console.log(infoArr)
     infoArr.forEach((el, index) => {
-        detect(el['artist'], el['song'])
+        // if (index < 50000) {
+            detect(el['artist'], el['song'])
+        // }
     })
 })
 
 async function detect(artist, song) {
-    let res = await axios.post('https://api.xiaoniangao.cn/plp/searchall', {
-        "token": "39bc56ad2cf446f28e94aa3c1df31951",
-        "txt": song,
-        "offset": 0,
-        "limit": 30,
-        "code_ver": "5.8.20",
-        "uid": "5277c4ec-4ee1-473a-8929-41d817d57eca",
-        "proj": "in"
-    })
-    if (res.data.data && res.data.data.list.length > 0) {
-        const list = res.data.data.list
-        for (let i = 0; i < 100; i++) {
-            if (list[i] && list[i]['music']['name'].indexOf(song) !== -1 && list[i]['music']['singer'].indexOf(artist) !== -1) {
-                console.log(`${artist} - ${song}`)
-                // return `${artist} - ${song}`
+    try {
+        let res = await axios.post('https://api.xiaoniangao.cn/plp/searchall', {
+            "token": "39bc56ad2cf446f28e94aa3c1df31951",
+            "txt": song,
+            "offset": 0,
+            "limit": 30,
+            "code_ver": "5.8.20",
+            "uid": "5277c4ec-4ee1-473a-8929-41d817d57eca",
+            "proj": "in"
+        })
+        if (res.data.data && res.data.data.list.length > 0) {
+            const list = res.data.data.list
+            for (let i = 0; i < 100; i++) {
+                if (list[i] && list[i]['music']['name'].indexOf(song) !== -1 && list[i]['music']['singer'].indexOf(artist) !== -1) {
+                    console.log(`${artist} - ${song}`)
+                    // return `${artist} - ${song}`
+                }
             }
+
         }
-        
+    } catch (e) {
+        // console.log(e)
     }
+    
 }
 
 
